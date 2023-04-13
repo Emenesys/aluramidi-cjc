701165f2177047754aabe8129a74252678c572dd
CLEBER JOSE COSTA <48730510+Emenesys@users.noreply.github.com>
2023-04-13 17:58:23 -0300

    atualizaçao do laço com For

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 };
-
+//busca os elementos com a classe "tecla"
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+//laço de repeticação
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`;//Template String
     
     tecla.onclick = function() {
         tocaSom(idAudio);
         }
-    contador += 1;
 
       
 }
